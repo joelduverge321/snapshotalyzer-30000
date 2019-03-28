796df63c3d8b9ffd2eb05e5b24e873786a8eb418
Joel Duverge <joelduverge321@gmail.com>
2019-03-28 09:52:37 -0500

    Updated instances to reboot

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -135,7 +135,8 @@ def list_instances(project):
             )))
 
     return 
-    
+
+#REBOOTING AN INSTANCE#  
 @instances.command('reboot')
 @click.option('--project', default=None,
     help="Only instances for project (tag Prject:<name>)")
@@ -145,11 +146,15 @@ def reboot_instances(project):
     instances = filter_instances(project) 
 
     for i in instances:
-        print(" Rebooting {0}...".format(i.id))
-    else:
-        print(" Could not reboot {0}. ".format(i.id))
-    
+        print("Rebooting {0}...".format(i.id))
+        try: 
+            i.reboot()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not reboot {0}. ".format(i.id) + str(e))
+        continue
+
     return
+
 
 ##STOPPING AN EC2 INSTANCE###
 @instances.command('stop')
